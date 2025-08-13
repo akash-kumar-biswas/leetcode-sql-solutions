483ee68aba7f69ee7e7eb784311ed135e66b32e2
Akash Biswas <akash41bt@gmail.com>
2025-08-14 01:45:59 +0600

    solve LeetCode 183: using nested query

--- 183-customers-who-never-order.sql
+++ 183-customers-who-never-order.sql
@@ -1,3 +1,3 @@
 /* Write your PL/SQL query statement below */
 
-select c.name Customers from Customers c left join Orders o on c.id = o.customerId where o.customerId is null;
+select c.name Customers from Customers c where c.id not in (select c.id Customers from Customers c inner join Orders o on c.id = o.customerId);
