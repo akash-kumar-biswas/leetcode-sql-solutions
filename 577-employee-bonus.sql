28ebf6950b7a8803990cb15c452436e5dc074c2a
Akash Biswas <akash41bt@gmail.com>
2025-08-14 18:37:20 +0600

    solve LeetCode 577: using left join

--- 577-employee-bonus.sql
+++ 577-employee-bonus.sql
@@ -1,3 +1,3 @@
 /* Write your PL/SQL query statement below */
 
-select name, bonus from (select e.name, bonus from Employee e left join Bonus b on e.empId = b.empId) where bonus < 1000 or bonus is null;
+select e.name, b.bonus from Employee e left join Bonus b on e.empId = b.empId where b.bonus < 1000 or b.bonus is null;
